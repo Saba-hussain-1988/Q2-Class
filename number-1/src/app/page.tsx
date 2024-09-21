72c22b5841517c687af145481fa687d9a2f0659a
Saba Hussain <saba.mar88@gmail.com>
2024-09-21 20:22:35 +0500

    save changes

--- number-1/src/app/page.tsx
+++ number-1/src/app/page.tsx
@@ -8,16 +8,23 @@ export default function Home() {
     <div className="home">
 
       <div className="child">
-        <h1>Assalam U Alaekum...!</h1>
+        <h1><b>Assalam U Alaekum...!</b></h1>
         <p>I'm <strong><a target="blank" href="mailto:sabamustafa27.88@gmail.com">S.R.Chohan</a></strong>.<br/>
         I'm a student of <abbr title="Governor Initiative Artificial Intelligence Course">GIAIC</abbr>.<br/>
         I wanna be a good Developer.</p>
+
+        <div>
+          <h1><b>S.R.Chohan</b></h1>
+          <p>Under Graduate(Economics)</p>
+        </div>
       </div>
 
-      <div className="child">
+
+      <div className="img-cont">
         <img src={profile.src}
         width={250}
-        height={250}/>
+        height={250}
+        className="pic"/>
       </div>
 
     </div>
